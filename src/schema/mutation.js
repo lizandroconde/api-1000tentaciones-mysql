98e08b9b02aceaee81ef0de65bfd91fd3179283f
Lizandro <lizandroconde@gmail.com>
2021-01-02 14:49:45 -0500

    actualizacion

--- src/schema/mutation.js
+++ src/schema/mutation.js
@@ -2,14 +2,14 @@
 import {GraphQLObjectType} from 'graphql'
 
 // App Imports
-import * as reserva from './reserva/fields/mutations'
+import * as categoria from './categoria/fields/mutations'
 // Mutation
 const mutation = new GraphQLObjectType({
   name: 'mutations',
   description: '...',
 
   fields: {
-  ...reserva
+  ...categoria
     
   }
 })
