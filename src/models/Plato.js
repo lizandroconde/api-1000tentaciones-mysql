887266f66a5046a328d96af38b11c6ef36b3b642
Lizandro <lizandroconde@gmail.com>
2021-01-02 16:15:27 -0500

    prueba

--- src/models/Plato.js
+++ src/models/Plato.js
@@ -47,7 +47,7 @@ export default (sequelize, DataTypes) => {
             
             Plato.hasMany(models.Tipo,{
                 as: "Tipos",foreignKey:{
-                    name:'_id',
+                    name:'_idmenu',
                     field:'IdTipo'
                 }
             })
